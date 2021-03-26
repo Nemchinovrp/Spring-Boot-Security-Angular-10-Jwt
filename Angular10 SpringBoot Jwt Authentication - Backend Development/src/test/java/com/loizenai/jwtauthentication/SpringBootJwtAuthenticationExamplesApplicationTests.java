4b30169fa989bc5d22a4574c1c7dfc53b2572030
nemchinovrp <nemchinovrp@yandex.ru>
2021-03-26 13:13:58 +0800

    init commit

---
+++ Angular10 SpringBoot Jwt Authentication - Backend Development/src/test/java/com/loizenai/jwtauthentication/SpringBootJwtAuthenticationExamplesApplicationTests.java
@@ -0,0 +1,9 @@
+package com.loizenai.jwtauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtAuthenticationExamplesApplicationTests {
+
+}
